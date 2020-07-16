ac34c3bd082b8f1c76b961d1975449904a00937e
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-15 23:13:08 -0400

    updated controller to add company to list and reroute

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,8 +1,50 @@
 class CompaniesController < ApplicationController
+ # before_action :set_company, only: [:show]
     def index
       @companies = Company.all
     end
+  
     def show
-        @company = Company.find(params[:id])
+      @company = Company.find(params[:id])
+      #@company.lists.build(name: "First Ingredient")
+      #@companylist = CompanyList.new
+      @lists = List.all
     end
+  
+    def new
+      @company = Company.new
+    end
+  
+      def create
+          byebug
+          #@company = company.create({name: params[:company][:name], user_id: 1}) #must set up current user and company params properly
+          @company = company.create(company_params)
+          if @company.save
+        redirect_to company_path(@company)
+      else
+        render :new
+      end
+    end
+  
+    def update
+      @company = Company.find(params[:id])
+      @list = params[:company][:list_ids]
+      @company.update(company_params)
+      redirect_to list_path(@list)
+    end
+  
+    def edit
+      @company = company.find(params[:id])
+      end
+  
+      def destroy
+          @company = company.find(params[:id])
+          @company.destroy
+          redirect_to companys_url
+        end
+      
+        def company_params
+          params.require(:company).permit(:list_ids)
+        end
+
 end
