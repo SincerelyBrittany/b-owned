040dea1838a043bc80db21b138aff8d92f523da1
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-15 23:15:19 -0400

    removes accepts_nested_attributes_for :companies

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,6 +5,7 @@ class List < ApplicationRecord
     belongs_to :user
     has_many :company_lists
     has_many :companies, through: :company_lists
+    #accepts_nested_attributes_for :companies
     def user_name=(name)
         self.user = User.find_or_create_by(name: name)
       end
