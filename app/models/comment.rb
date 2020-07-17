e4c8f36cfdc02b85c3040d4030ec5a955332aad1
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-16 20:01:40 -0400

    adds comment_count method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :company
-    def user_name
-        self.user.name
+    def comment_count
+        self.comments.size
     end
 end
