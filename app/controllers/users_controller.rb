0b251ff8dbced1053cc9f5fb7a9cb462020dca28
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-16 20:06:00 -0400

    adds email to the user_params so that it can be passed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
       end
     
     def user_params
-        params.require(:user).permit(:username)
+        params.require(:user).permit(:username, :email)
     end
 
 end
