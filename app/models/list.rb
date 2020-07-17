4d2d5a0844d51241400193fd79b6acfadcc50573
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-16 20:00:41 -0400

    adds list_id=(ids) method

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,14 +3,14 @@ class List < ApplicationRecord
      #list = List.create
      #list.errors.full_messages
     belongs_to :user
-    has_many :company_lists
+    has_many :company_lists, dependent: :destroy # When you delete this list, delete all of the company_list 
+    #has_many :company_lists, dependent: :delete_all
     has_many :companies, through: :company_lists
     #accepts_nested_attributes_for :companies
-    def user_name=(name)
-        self.user = User.find_or_create_by(name: name)
+    def list_ids=(ids)
+      ids.each do |id|
+        list = List.find(id)
+        self.lists << list
       end
-    
-    def user_name
-         self.user ? self.user.name : nil
-    end
+  end
 end
