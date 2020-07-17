57c985cf35418e705e278780970dd4e00a8a5005
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-17 11:18:41 -0400

    updates the comment params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,8 +7,8 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    #params.require(:comment).permit(:content, :company_id, :user_id, user_attributes:[:username])
-   params.require(:comment).permit(:content, :company_id, :user_id)
+    params.require(:comment).permit(:content, :company_id, :user_id, user_attributes:[:username])
+   #params.require(:comment).permit(:content, :company_id, :user_id)
   
   end
 end
