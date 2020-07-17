23fe70fee6e8af787c3ac206683193087d9595aa
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-16 20:03:04 -0400

    adds comment_count  method

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,16 @@
 class Company < ApplicationRecord
     has_many :comments
     has_many :users, through: :comments
+
     has_many :company_lists
     has_many :lists, through: :company_lists
+
+    # def company_attributes=(company_attributes)
+    #     company_attributes.values.each do |company_attribute|
+    #       company = Company.find_or_create_by(company_attribute)
+    #       self.list_companies.build(company: company)
+    #     end
+    #   end
 
     def comment_count
         self.comments.size
@@ -10,6 +18,7 @@ class Company < ApplicationRecord
 
     has_many :favorites
     has_many :users, through: :favorites
+
     def favorite_count
         self.favorites.size
     end
