71cb49211ac52806b5e87c4cdc9138042be6bc37
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-20 14:06:47 -0400

    adds the favorites link_to page to show all users that favorites a pcompany

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,25 @@
 class FavoritesController < ApplicationController
     def create
-       
-        @favorite = Favorite.create(fav_params)
+        @favorite = Favorite.new(fav_params)
+        # if Favorite.where(user_id: @favorite.user_id, company_id: @favorite.company_id)
+            #see if liked is true 
+            #if liked is true then redirect to company page with message saying you already follow/favorited
+        #else 
+         #if liked is false then save favorite and redirect to user page showing follow.    
         if @favorite.save
-            @favorite.user
+            redirect_to @favorite.user
         else
-            @favorite.company
+            redirect_to @favorite.company
         end
-	end
+    end
+    
+    def index
+        # byebug
+        @company = Company.find(params[:company_id])
+        #raise params.inspect
+    end 
 
     def fav_params
-        params.require(:favorite).permit(:user_id, :company_id)
+        params.require(:favorite).permit(:user_id, :company_id, :liked)
     end
 end
