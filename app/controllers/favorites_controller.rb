5a21fe24e993f91b45df93b561d2ad80e033c419
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-21 14:43:43 -0400

    updates favorites create method to create a favorite or remove a favorite if found

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,7 +5,6 @@ class FavoritesController < ApplicationController
         if @favorite.save
             redirect_to @favorite.user
         else 
-            
             @fav = Favorite.find_by(user_id: @favorite.user_id, company_id: @favorite.company_id)
             @fav.destroy
             redirect_to @favorite.company
@@ -21,8 +20,6 @@ class FavoritesController < ApplicationController
         @company = Company.find(params[:company_id])
         #raise params.inspect
     end 
-
-   
 
     def fav_params
         params.require(:favorite).permit(:user_id, :company_id)
