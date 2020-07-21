ad0cc1e70711fd6e68ae27f018198d6012233ff3
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-21 14:40:41 -0400

    validates_uniqueness_of

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,18 +2,37 @@ class Favorite < ApplicationRecord
     belongs_to :company
     belongs_to :user
 
+     validates_uniqueness_of :user_id, :scope => :company_id
    
     # def liked?
     #     self.favorite = Favorite.where(user_id: @favorite.user_id, company_id: @favorite.company_id)
     # end
 
-    def liked?
-        self.favorite == true
-    end 
+    # def like_count
+    #     byebug
+    #     favovite = self.liked == true
+    #     favovite.count
+    # end 
 
-    def unliked?
-        self.favorite == false
-    end 
+    # def self.liked(fav_params)
+    #     if self.where(user_id: fav_params[:user_id], company_id: fav_params[:company_id]).present?
+    #         word = where(user_id: fav_params[:user_id], company_id: fav_params[:company_id])
+    #         if word.where("liked = true")
+    #             word.update(:liked => false)
+    #             @favorite = word
+    #         else 
+    #             word.where("liked = false")
+    #             word.update(:liked => true)
+    #             @favorite = word
+    #         end
+    #     else 
+    #         self.new(fav_params)
+    #     end
+    # end
+
+    # def unliked?
+    #     self.liked == false
+    # end 
 
 
 
