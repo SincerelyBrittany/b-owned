64e134c263fb3505ffd7dbfb29d9be590ff76fe8
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-24 17:03:38 -0400

    adds user routes to edit & update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-    def show 
-        byebug
-    end 
+    def show
+        @user = User.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update]
   root :to => "companies#index"
   resources :lists
   resources :companies
