675d8583a13f6921deae80269a1c9b1dcf1edc6a
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-25 18:28:44 -0400

    adds user authorize and authenicate methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+
+    def logged_in?
+        !!current_user
+      end
+    
+      def authenticate
+        redirect '/login' if !logged_in?
+      end
+    
+      def authorize_user(user)
+        authenticate
+        redirect_to companies_path if user != current_user
+      end
+    
+      def authorize_admin
+        authenticate
+        redirect_to companies_path if current_user.admin == false
+      end
+    
+      # def authorize_user(user)
+      #   if user != current_user 
+      #     redirect '/companies' 
+      #   elsif user != current_user && current_user.owner == false
+      # end 
+    
+      def authorize(list)
+         authenticate
+         redirect_to list_path(list.id) if list.user != current_user && current_user.admin != true
+      end
 end
