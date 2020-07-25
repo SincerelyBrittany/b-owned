2d5de36cbdef238c21b2a132bef7f3534e521686
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-25 18:09:34 -0400

    creates signup view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,9 @@ class UsersController < ApplicationController
     
       def create
         @user = User.new(user_params)
-        if @user.save
-          redirect_to @user
+        if @user.save 
+            session[:user_id] = @user.id #logs in the user -- tells app that user is logged in
+            redirect_to @user
         else
           render :new
         end
