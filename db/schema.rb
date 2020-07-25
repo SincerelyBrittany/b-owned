f1030238d3ddfe868f3d9a8946ec552c624f212b
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-25 17:35:44 -0400

    adds bycrypt gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_223711) do
+ActiveRecord::Schema.define(version: 2020_07_25_213352) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_07_14_223711) do
     t.text "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
