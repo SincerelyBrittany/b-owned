bbbe107abdd124e5f33b27ddf3e6cc656598078c
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-29 14:31:24 -0400

    adds validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,36 @@
 class User < ApplicationRecord
     has_secure_password
+    # validate :password_lower_case
+    # validate :password_uppercase
+    # validate :password_special_char
+    # validate :password_contains_number
+  
+    # def password_uppercase
+    #   return if !!password.match(/\p{Upper}/)
+    #   errors.add :password, ' must contain at least 1 uppercase '
+    # end
+  
+    # def password_lower_case
+    #   return if !!password.match(/\p{Lower}/)
+    #   errors.add :password, ' must contain at least 1 lowercase '
+    # end
+  
+    # def password_special_char
+    #   special = "?<>',?[]}{=-)(*&^%$#`~{}!"
+    #   regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
+    #   return if password =~ regex
+    #   errors.add :password, ' must contain special character'
+    # end
+  
+    # def password_contains_number
+    #   return if password.count("0-9") > 0
+    #   errors.add :password, ' must contain at least one number'
+    # end
+
+
     validates :username, uniqueness: true
     validates :email, uniqueness: true
-    has_many :comments
+    has_many :comments, dependent: :destroy
     has_many :companies, through: :comments
     has_many :favorites
     has_many :lists
