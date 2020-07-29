a441bcdf7311c01d2427f406b994ec2c7c4fad9e
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-29 13:48:38 -0400

    adds nested resources and cleans up routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,13 @@ Rails.application.routes.draw do
   resources :lists
   resources :users
   resources :companies
-  resources :favorites
-  resources :comments
-  resources :company_lists
+  resources :favorites, only: [:create, :new]
+  resources :comments, only: [:create, :new]
+  resources :company_lists, only:[:create, :destroy]
 
   resources :companies do 
-    resources :favorites
-  end 
-  
+    resources :favorites, only: [:index]
+    resources :comments, only: [:edit, :update, :destroy]
+  end   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
