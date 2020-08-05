0d8278f850512fc1ea71e79596d62bcef35f3c6b
SincerelyBrittany <sincerelybr@gmail.com>
2020-08-05 16:06:45 -0400

    allows users sign in as a user using a checkbox option

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,10 @@ class UsersController < ApplicationController
     
       def create
         @user = User.new(user_params)
+        # byebug
+        if params[:user][:owner]
+              @user.owner = true
+        end 
         if @user.save 
             session[:user_id] = @user.id #logs in the user -- tells app that user is logged in
             redirect_to @user
